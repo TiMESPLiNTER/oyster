199fd783d4ca89b4557866d898dc148b1a03ab9b
TiMESPLiNTER <github@timesplinter.ch>
2018-12-03 08:00:06 +0100

    Vim works now

--- src/Executor.php
+++ src/Executor.php
@@ -12,18 +12,29 @@ use Timesplinter\Oyster\Output\OutputInterface;
 class Executor
 {
 
+    const MODE_TTY = 'tty';
+
+    const MODE_PIPE = 'pipe';
+
     /**
      * @var OutputInterface
      */
     private $output;
 
     /**
+     * @var string
+     */
+    private $mode;
+
+    /**
      * Executor constructor.
      * @param OutputInterface $output
+     * @param string          $mode
      */
-    public function __construct(OutputInterface $output)
+    public function __construct(OutputInterface $output, string $mode)
     {
         $this->output = $output;
+        $this->mode = $mode;
     }
 
     /**
@@ -39,11 +50,21 @@ class Executor
         $stream = null;
         $streamContent = null;
 
-        $descriptorSpec = [
-            0 => ['pipe', 'r'],
-            1 => ['pipe', 'w'],
-            //2 => ['file', "./error-output.txt", "a"]
-        ];
+        if (self::MODE_TTY === $this->mode) {
+            $descriptorSpec = [
+                0 => ['file', '/dev/tty', 'r'],
+                1 => ['file', '/dev/tty', 'w'],
+                2 => ['file', '/dev/tty', 'w'],
+            ];
+        } elseif (self::MODE_PIPE === $this->mode) {
+            $descriptorSpec = [
+                0 => ['pipe', 'r'],
+                1 => ['pipe', 'w'],
+                //2 => ['file', "./error-output.txt", "a"]
+            ];
+        } else {
+            throw new \RuntimeException(sprintf('Unknown mode "%s"', $this->mode));
+        }
 
         //var_dump($command . ' ' . implode(' ', $arguments));
 
@@ -59,10 +80,22 @@ class Executor
             /*fwrite($pipes[0], '<?php echo "HELLO FROM TEST"; ?>'); // here directly the code of child.php
             fclose($pipes[0]);*/
 
-            $this->output->write(stream_get_contents($pipes[1]));
-            fclose($pipes[1]);
+            while (true === ($info = proc_get_status($process))['running']) {
+
+            }
+
+            file_put_contents('returnCodes.txt', $command . ' -> ' .var_export($info, true) . PHP_EOL . PHP_EOL, FILE_APPEND);
+
+            if (self::MODE_PIPE === $this->mode) {
+                $this->output->write(stream_get_contents($pipes[1]));
+                fclose($pipes[1]);
+            }
 
             proc_close($process);
+
+            /*if (self::MODE_TTY === $this->mode) {
+                $this->output->write("\033[1J");
+            }*/
         }
 
         return 0;
